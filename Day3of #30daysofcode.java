fe6b8bbbd2edb6d0a117b10bb3bc87fa4ec54b7c
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2022-08-20 15:26:54 +0530

    Create Day3of #30daysofcode.java

---
+++ Day3of #30daysofcode.java
@@ -0,0 +1,43 @@
+//Problem: Two Sums
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> map=new HashMap();
+
+        
+
+        
+
+        for(int i=0;i<nums.length;i++){ //Filling HashMap 
+
+            map.put(nums[i],i);  
+
+        }
+
+        
+
+        for(int i=0;i<nums.length;i++){
+
+            int num=nums[i];
+
+            int rem=target-num;
+
+            if(map.containsKey(rem)){
+
+                int index =map.get(rem);
+
+                if(index==i) continue;
+
+                return new int[]{i,index};
+
+            }
+
+        }
+
+        return new int[]{};
+
+    }
+
+}
+
