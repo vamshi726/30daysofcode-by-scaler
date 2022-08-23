0c89c6a40421074a812e6e4ec35b8a73b0f74fe7
Vamshi Muluguri <63087230+vamshi726@users.noreply.github.com>
2022-08-23 20:37:16 +0530

    Create Day6of30daysofcode.java

---
+++ Day6of30daysofcode.java
@@ -0,0 +1,29 @@
+//Problem: Move Zeroes 
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        if(n==0||n==1){
+            return;
+        }
+        
+        int left=0,right=0,temp;
+        while(right<n){
+            if(nums[right]==0){
+                ++right;
+            }
+            else{
+                temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                ++left;
+                ++right;
+            }
+        }
+    }
+}
+
+/*
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+*/
