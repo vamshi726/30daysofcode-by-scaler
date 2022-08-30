2b8e88f6d7d646f2b70955f8f2d04224d21964ce
Vamshi Muluguri <vamshimulgoori2001@gmail.com>
2022-08-30 21:24:36 +0530

    Create Day 13 of #30daysofcode Print all the duplicates in the input string.java

---
+++ Day 13 of #30daysofcode Print all the duplicates in the input string.java
@@ -0,0 +1,31 @@
+//Problem: Reverse Words in a String
+
+class Solution {
+    public String reverseWords(String s) {
+        int i=s.length()-1;
+        
+        String ans="";
+        
+        while(i>=0){ 
+        while(i>=0 && s.charAt(i) == ' ') i--;
+        int j=i;
+        
+        if(i<0) break;
+        
+        while(i>=0 && s.charAt(i)!=' ') i--;
+
+        if(ans.isEmpty()){
+            ans=ans.concat(s.substring(i+1,j+1));
+            }
+        else{
+            ans=ans.concat(" "+s.substring(i+1,j+1));
+        }
+    } return ans;
+        }
+}
+
+/*
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+
+*/
