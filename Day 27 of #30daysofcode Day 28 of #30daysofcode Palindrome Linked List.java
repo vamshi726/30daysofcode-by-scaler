b95d0952b519eecbd7d1aceaa5c80bb02faf17ce
Vamshi Muluguri <vamshimulgoori2001@gmail.com>
2022-09-14 17:51:46 +0530

    Create Day 27 of #30daysofcode Day 28 of #30daysofcode Palindrome Linked List.java

---
+++ Day 27 of #30daysofcode Day 28 of #30daysofcode Palindrome Linked List.java
@@ -0,0 +1,45 @@
+//Problem: Palindrome Linked List
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+         if(head==null || head.next==null)
+            return true;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        //odd node
+        if (fast != null) {
+            slow = slow.next;
+        }
+        slow = reverse(slow);
+        fast = head;
+
+        while (slow != null) {
+            if (fast.val != slow.val) {
+                return false;
+            }
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev; 
+        
+    }
+} 
+/*
+Input: head = [1,2,2,1]
+Output: true
+*/
